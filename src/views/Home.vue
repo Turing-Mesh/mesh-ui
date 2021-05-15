5d19175e19db101bea17efb6f8873fe1694c8df7
Nikki <piknik@me.com>
2021-05-15 06:16:48 -0600

    add some css spinning stuff

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/mesh-logo.png">
+    <router-link to="/about">
+      <img class="rotating" alt="Vue logo" src="../assets/mesh-logo.png">
+    </router-link>
     <HelloWorld msg="Welcome to Mesh"/>
   </div>
 </template>
@@ -16,3 +18,13 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+.rotating {
+  width: 150px;
+  transition: transform 3s ease-in-out;
+}
+.rotating:hover {
+  transform: rotateZ(360deg);
+}
+</style>
