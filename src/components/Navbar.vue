38fc6cd5c6dc64350b242c65c4b0035ba7d7b3ac
Nikki <piknik@me.com>
2021-05-15 15:06:52 -0600

    Add calendar to date

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -5,7 +5,10 @@
         <img class="logo" alt="Mesh logo" src="../assets/mesh-logo.png">
       </div>
       <div class="link-container">
-        <span class="date">{{ date }}</span> |
+        <span class="date">
+          <img class="calendar" alt="Calendar icon" src="../assets/calendar.svg">
+          {{ date }}
+        </span> |
         <span class="name">Hi, Nikki</span> |
         <router-link to="/">Home</router-link> |
         <router-link to="/about">About</router-link>
