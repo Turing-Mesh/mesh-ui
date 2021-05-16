99d059efaf3a814339ea9b16e6c610edbdf00dd5
Katie Bee <knees4bees@gmail.com>
2021-05-16 12:27:53 -0600

    Add dummy text and temp styling

--- src/components/StudentFeedback.vue
+++ src/components/StudentFeedback.vue
@@ -1,12 +1,34 @@
 <template>
   <section class="feedback-container">
-    <div class="categories">
-      <p>JavaScript</p>
-      <p>React</p>
-      <p>Professionalism</p>
+    <div class="feedback-container__category">
+      <p class="feedback-container__category--label">JavaScript</p>
+      <div class="progress-bar" id="bar1">3.5</div>
+      <p class="feedback-container__category--comments">Solid work here! Blah blah.</p>
     </div>
-    <div class="comments">
-      You like cats because they are fat and fluffy inspect anything brought into the house, yet flee in terror at cucumber discovered on floor ask for petting trip on catnip. Run off table persian cat jump eat fish. Loves cheeseburgers meowing chowing and wowing and human is behind a closed door, emergency! abandoned! meeooowwww!!!.
+    <div class="feedback-container__category">
+      <p class="feedback-container__category--label">React</p>
+      <div class="progress-bar" id="bar2">2.5</div>
+      <p class="feedback-container__category--comments">Some comments here about this category.</p>
+    </div>
+    <div class="feedback-container__category">
+      <p class="feedback-container__category--label">Professionalism</p>
+      <div class="progress-bar" id="bar3">3</div>
+      <p class="feedback-container__category--comments">Looking good. Just make sure to foo!</p>
+    </div>
+    <div class="feedback-container__category">
+      <p class="
+        feedback-container__category--label
+        feedback-container__category--label-overall
+      ">
+        Overall
+      </p>
+      <div class="progress-bar" id="bar-average">3</div>
+      <p class="
+        feedback-container__category--comments
+        feedback-container__category--comments-overall
+      ">
+        You like cats because they are fat and fluffy inspect anything brought into the house, yet flee in terror at cucumber discovered on floor ask for petting trip on catnip. Run off table persian cat jump eat fish. Loves cheeseburgers meowing chowing and wowing and human is behind a closed door, emergency! abandoned! meeooowwww!!!.
+      </p>
     </div>
   </section>
 </template>
@@ -17,5 +39,18 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-
+  .feedback-container {
+  }
+  .feedback-container__category {
+    width: 400px;
+    height: 40px;
+  }
+  .feedback-container__category--label {
+    display: inline;
+  }
+  .progress-bar {
+    display: inline;
+    width: 100px;
+    background-color: aqua;
+  }
 </style>
