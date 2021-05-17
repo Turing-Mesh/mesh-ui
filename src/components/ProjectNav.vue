53646a1863e33dd79b702b7b8d0085faccc24890
Jessica Justice <jessbjust@gmail.com>
2021-05-17 00:32:26 -0600

    Add router link to one Project tab

--- src/components/ProjectNav.vue
+++ src/components/ProjectNav.vue
@@ -1,14 +1,34 @@
 <template>
-  <div class="projects-container">
-    <h1 class="project">Project 1</h1>
-    <h1 class="project">Project 2</h1>
-    <h1 class="project">Project 3</h1>
-    <h1 class="project">Project 4</h1>
-  </div>
+  <nav class="projects-container">
+    <router-link to="/Project-1" class="project">Project 1</router-link>
+    <h2 class="project">Project 2</h2>
+    <h2 class="project">Project 3</h2>
+    <h2 class="project">Project 4</h2>
+    <router-view />
+  </nav>
 </template>
 
 <script>
+
 </script>
 
-<style>
+<style lang="scss" scoped>
+@import '@/styles/styles.scss';
+
+.projects-container {
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-template-rows: 1fr;
+}
+
+.project {
+  border: 1px solid black;
+  padding: 10px;
+  text-align: center;
+
+  &:hover {
+    background-color: $turing-yellow;
+  }
+}
+
 </style>
