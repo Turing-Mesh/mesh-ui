985e50ebaed1b1537a54c5d14a6de6cf3e83b731
Nikki <piknik@me.com>
2021-05-18 07:40:39 -0600

    Change a word

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,5 +1,5 @@
 <template>
-  <footer id="footer">Created by Ben, Genevieve, Jessica 🦑, Jesus, Katie 🐢, and Nikki 👖.</footer>
+  <footer id="footer">Built by Ben, Genevieve, Jessica 🦑, Jesus, Katie 🐢, and Nikki 👖.</footer>
 </template>
 
 <script>
