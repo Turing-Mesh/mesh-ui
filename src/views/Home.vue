f66520b71f22858de1889e0f42ae5cd849c67046
Nikki <piknik@me.com>
2021-05-20 12:49:52 -0600

    Finish up rebase conflicts

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -9,10 +9,10 @@
         <Signup />
       </div>
 
-      <div v-else>
+      <div v-else class="landing">
         <h1 class="s-h1" id="hello">Welcome, Turing student!</h1>
         <img class="turing-logo" src="https://assets.turing.edu/turing-school-mark-gray.png" alt="Turing logo">
-      </div>)
+      </div>
     </section>
   </div>
 </template>
