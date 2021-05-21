afe14ce9ebf4b1a1772844783ccd51368a0dd665
Katie Bee <knees4bees@gmail.com>
2021-05-20 21:36:12 -0600

    Create skeleton functionality

--- src/components/Category.vue
+++ src/components/Category.vue
@@ -3,8 +3,8 @@
     <p class="feedback-container__category--label">
       <span class="s-h2">{{ projectFeedback.rubric_category_name }}</span>
     </p>
-    <div class="progress-bar" id="bar1">{{ projectFeedback.score }}</div>
-    <p class="feedback-container__category--comments s-text-body">
+    <div class="progress-bar" id="bar1" ref="bar">{{ projectFeedback.score }}</div>
+    <p class="feedback-container__category--comments" @click="makeBigBar" s-text-body">
       {{ projectFeedback.comment }}
       Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad blanditiis, cupiditate deserunt, esse eum illo, iure labore nesciunt officia officiis quidem ullam voluptate voluptatum! Consectetur corporis dolorum ducimus ex repellendus.
     </p>
@@ -15,6 +15,12 @@
 export default {
   props: {
     projectFeedback: Object
+  },
+  methods: {
+    makeBigBar () {
+      console.log('hello!')
+      console.log(this.$refs.bar)
+    }
   }
 }
 </script>
