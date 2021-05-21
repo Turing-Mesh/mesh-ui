3895cc4368e827e0356ab2f599aab1eca98d09d0
Nikki <piknik@me.com>
2021-05-21 13:13:49 -0600

    making notes from Matt

--- src/store/index.js
+++ src/store/index.js
@@ -5,6 +5,8 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    // currentMod in state after initial auth
+    // allModules stored here also then currentMod changes when button click
   },
   mutations: {
   },
@@ -13,3 +15,11 @@ export default new Vuex.Store({
   modules: {
   }
 })
+
+// getter to get data
+// do api call once if small
+// per module if it's medium amount
+// in actions -- trigger dispatch to store to get modules
+// load first module by default -- currentModule ??
+// what does user want, what is their intent when using
+// mapState. mapGetters
