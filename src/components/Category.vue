81b5134ad42ec7a09d75cde47856d44867e6562f
Katie Bee <knees4bees@gmail.com>
2021-05-22 12:49:12 -0600

    Refine style and placement of score in progress bar

--- src/components/Category.vue
+++ src/components/Category.vue
@@ -4,8 +4,10 @@
       <span class="s-h2">{{ projectFeedback.rubric_category_name }}</span>
     </p>
     <div class="progress-bar" :style="{ width: projectFeedback.score * 80 + 'px'}">
-      <div class="progress-bar__score">
-        {{ projectFeedback.score }}
+      <div class="progress-bar__score-wrapper">
+        <div class="progress-bar__score-wrapper--number">
+          {{ projectFeedback.score }}
+        </div>
       </div>
     </div>
     <p class="feedback-container__category--comments" s-text-body>
