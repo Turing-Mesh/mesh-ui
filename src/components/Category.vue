ec0d58c780ad9c7a8dbaf316e41587ce7717d5af
Katie Bee <knees4bees@gmail.com>
2021-05-21 22:16:57 -0600

    Draft progress bars - a big mess

--- src/components/Category.vue
+++ src/components/Category.vue
@@ -3,10 +3,12 @@
     <p class="feedback-container__category--label">
       <span class="s-h2">{{ projectFeedback.rubric_category_name }}</span>
     </p>
-    <div class="progress-bar" id="bar1" ref="bar">
-      <span class="progress-bar__score">
+    <!-- <div class="progress-bar" id="bar-{{projectFeedback.id}}" ref="bar"> -->
+    <div class="progress-bar" :id="projectFeedback.id" :ref="projectFeedback.id">
+    <!-- <div class="progress-bar" :id="projectFeedback.id" ref="bar"> -->
+      <div class="progress-bar__score">
         {{ projectFeedback.score }}
-      </span>
+      </div>
     </div>
     <p class="feedback-container__category--comments" @click="makeBigBar" s-text-body>
     <!-- <p class="feedback-container__category--comments" @click="testing" s-text-body> -->
@@ -21,14 +23,41 @@ export default {
   props: {
     projectFeedback: Object
   },
+  created () {
+    console.log('Category created')
+    console.log('projectFeedback.id: ', this.projectFeedback.id)
+    // const id = this.projectFeedback.id.toString()
+    // const elem = document.getElementById(`${id}`)
+    // const myRef = this.projectFeedback.id
+    const elem = this.$refs.projectFeedback.id
+    // const elem = document.getElementById(this.projectFeedback.id)
+    console.log('yo yo elems: ', elem)
+    // console.log('width of element:', elem.style.width)
+    const newWidth = this.projectFeedback.score * 100
+    // this.$refs[elem].style.width = 100 * this.projectFeedback.score
+    elem.style.width = `${newWidth}px`
+    // elems.forEach(elem => {
+    //   const newWidth = this.projectFeedback.score * 100
+    //   elem.style.width = `${newWidth}px`
+    // elem.style.width = '500px'
+    // })
+  },
   methods: {
     makeBigBar () {
       console.log('hello!')
-      console.log(this.$refs.bar)
-      this.$refs.bar.style.width = 200
-      const elem = document.getElementById('bar1')
-      console.log('width of element:', elem.style.width)
-      elem.innerText = 'testing'
+      console.log('this.$refs.bar', this.$refs.bar)
+      // this.$refs.bar.style.width = 200
+      this.$refs.bar.style.width = 100 * this.projectFeedback.score
+      // const elem = document.getElementById('bar1')
+      const elems = document.getElementsByClassName('progress-bar')
+      console.log('elems: ', elems)
+      // console.log('width of element:', elem.style.width)
+      elems.forEach(elem => {
+        const newWidth = this.projectFeedback.score * 100
+        elem.style.width = `${newWidth}px`
+        // elem.style.width = '500px'
+      })
+      // elem.innerText = 'testing'
     },
     testing () {
       const elem = document.getElementById('bar1')
@@ -54,12 +83,8 @@ export default {
 <style lang="scss" scoped>
   @import '@/styles/styles.scss';
 
-  // .progress-bar {
-  //   display: inline;
-  //   width: 100px;
-  //   background-color: $turing-yellow;
-  //   margin-left: 15px;
-  //   font-size: 1.25rem;
-  //   font-weight: bold;
-  // }
+  .progress-bar__score {
+    margin: 0;
+    vertical-align: center;
+  }
 </style>
