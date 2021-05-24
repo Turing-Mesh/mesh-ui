6a402a4b6faf46328d257e80ea79b7f37e979ec0
Nikki <piknik@me.com>
2021-05-24 04:46:59 -0600

    add note fetch to store but need a project id first

--- src/store/index.js
+++ src/store/index.js
@@ -56,6 +56,17 @@ export default new Vuex.Store({
           context.commit('setAllModules', data)
         })
     }
+    // addNote (context, note) {
+    //   return fetch(`https://shrouded-citadel-55795.herokuapp.com/api/v1/students/1/student_projects/${this.$route.params.id}`, {
+    //     method: 'PATCH',
+    //     headers: {
+    //       'Content-Type': 'application/json'
+    //     },
+    //     body: JSON.stringify(data)
+    //   })
+    //     .then(response => response.json)
+    //     .then(data => console.log(data))
+    // }
   },
   modules: {
   },
