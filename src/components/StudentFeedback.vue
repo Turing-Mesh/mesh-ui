d8bcaf83702e3077fbb456676e9f9b9053a10578
Jessica Justice <jessbjust@gmail.com>
2021-05-25 04:06:57 -0600

    Clean up, commented out code for debugging sesh

--- src/components/StudentFeedback.vue
+++ src/components/StudentFeedback.vue
@@ -14,35 +14,6 @@
         <h1>{{ this.$store.state.form.data.attributes.rubric_template[1].rubric_category_name }}</h1>
         <h1>{{ this.$store.state.form.data.attributes.rubric_template[2].rubric_category_name }}</h1>
         <h1>{{ this.$store.state.form.data.attributes.rubric_template[3].rubric_category_name }}</h1>
-        <!-- <Project :project="this.project"/>
-        <section class="notes-container">
-          <p class="feedback-container__category--label">
-            <span class="s-h2">Student Notes</span>
-          </p>
-          <div class="current-notes">
-            <p>{{ studentNote }}</p>
-          </div>
-          <div class="form-container">
-            <button @click="toggleNoteForm" class="s-button s-button-secondary show form-container__item">
-              {{ showNoteForm ? 'Hide form' : 'Add new note' }}
-            </button>
-            <form @submit.prevent="AddNote" v-if="showNoteForm" class="form-container__item">
-              <div class="note form-container__item--note">
-                <textarea class="note__textarea"
-                          v-model="formData.note"
-                          rows="4"
-                          placeholder="Make notes for yourself here . . . "
-                          required
-                >
-                </textarea>
-              </div>
-              <div class="buttons form-container__item--buttons">
-                <button class="s-button s-button-primary-inverse reset" type="reset">Reset</button>
-                <button class="s-button s-button-primary add">Add</button>
-              </div>
-            </form>
-          </div>
-        </section> -->
       </div>
 
     <div v-else-if="this.project">
@@ -120,7 +91,6 @@ export default {
     },
     getForm () {
       this.$store.dispatch('getForm', { instructorId: 122, studentId: this.$store.state.currentStudent.attributes.user_id, modNum: this.$route.params.id, projectNum: this.$route.params.project_id })
-      console.log(this.$store.state.form)
     }
   },
   computed: {
