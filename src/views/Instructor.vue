0ace023461fa4029805134e50d5e581d258a5a3b
Nikki <piknik@me.com>
2021-05-25 08:38:11 -0600

    Add some structure to the instructor view

--- src/views/Instructor.vue
+++ src/views/Instructor.vue
@@ -1,10 +1,15 @@
 <template>
   <div class="instructor-container">
     <h1>Instructor View</h1>
-    <div class="main">
-      <ModuleNav :loggedIn="loggedIn" :style="instructorAuth"/>
-
-<!--      <section class="right-section">-->
+    <ModuleNav :loggedIn="loggedIn" :class="{ 'instructor-left-section': instructorAuth }"/>
+    <div class="instructor-main">
+      <ul>
+        <li class="student-name" v-for="(student, index) in students" :key="index">
+          {{ student }}
+        </li>
+      </ul>
+<!--      <section class="">-->
+<!--        <h1>is this the main ?</h1>-->
 <!--        <router-view-->
 <!--          :key="$route.path"-->
 <!--          :loggedIn="loggedIn"-->
@@ -20,6 +25,11 @@ import ModuleNav from '@/components/ModuleNav'
 
 export default {
   name: 'Instructor',
+  data () {
+    return {
+      students: ['One Student', 'Two Student', 'Three Student']
+    }
+  },
   props: {
     loggedIn: Boolean,
     authenticated: Boolean,
@@ -32,13 +42,28 @@ export default {
 </script>
 
 <style scoped>
-.left-section {
+.instructor-container {
+  width: 90%;
+  margin: 0 auto;
+}
+
+.instructor-main {
+  width: 100%;
+  height: 95vh;
+}
+
+.instructor-left-section {
+  /*background: purple;*/
   display: grid;
   grid-template-columns: repeat(4, 1fr);
-  width: 25%;
-  /*height: 40%;*/
+  width: 100%;
+  height: 60px;
   margin: 30px auto;
   text-align: center;
+}
+
+.student-name {
+  color: #4C4D4F;
 }
 
 </style>
