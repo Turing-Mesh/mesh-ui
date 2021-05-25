b79bee230ef7a7d5515cd6774e9c494d8c9b2440
Nikki <piknik@me.com>
2021-05-25 15:59:24 -0600

    remove the notes form from the instructor form view

--- src/components/InstructorFeedbackForm.vue
+++ src/components/InstructorFeedbackForm.vue
@@ -23,26 +23,26 @@
         <div class="current-notes">
           <p>{{ studentNote }}</p>
         </div>
-        <div class="form-container">
-          <button @click="toggleNoteForm" class="s-button s-button-secondary show form-container__item">
-            {{ showNoteForm ? 'Hide form' : 'Add new note' }}
-          </button>
-          <form @submit.prevent="AddNote" v-if="showNoteForm" class="form-container__item">
-            <div class="note form-container__item--note">
-              <textarea class="note__textarea"
-                        v-model="formData.note"
-                        rows="4"
-                        placeholder="Make notes for yourself here . . . "
-                        required
-              >
-              </textarea>
-            </div>
-            <div class="buttons form-container__item--buttons">
-              <button class="s-button s-button-primary-inverse reset" type="reset">Reset</button>
-              <button class="s-button s-button-primary add">Add</button>
-            </div>
-          </form>
-        </div>
+<!--        <div class="form-container">-->
+<!--          <button @click="toggleNoteForm" class="s-button s-button-secondary show form-container__item">-->
+<!--            {{ showNoteForm ? 'Hide form' : 'Add new note' }}-->
+<!--          </button>-->
+<!--          <form @submit.prevent="AddNote" v-if="showNoteForm" class="form-container__item">-->
+<!--            <div class="note form-container__item&#45;&#45;note">-->
+<!--              <textarea class="note__textarea"-->
+<!--                        v-model="formData.note"-->
+<!--                        rows="4"-->
+<!--                        placeholder="Make notes for yourself here . . . "-->
+<!--                        required-->
+<!--              >-->
+<!--              </textarea>-->
+<!--            </div>-->
+<!--            <div class="buttons form-container__item&#45;&#45;buttons">-->
+<!--              <button class="s-button s-button-primary-inverse reset" type="reset">Reset</button>-->
+<!--              <button class="s-button s-button-primary add">Add</button>-->
+<!--            </div>-->
+<!--          </form>-->
+<!--        </div>-->
       </section>
       <!--    </div>-->
   </section>
