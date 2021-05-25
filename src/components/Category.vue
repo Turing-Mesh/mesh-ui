eb4c8713ac47759a0366d739fd85bfce806bf193
Nikki <piknik@me.com>
2021-05-25 05:48:17 -0600

    Create and adjust container for overall scoring

--- src/components/Category.vue
+++ src/components/Category.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="feedback-container__category">
-    <p class="feedback-container__category--label">
-      <span class="s-h2">{{ projectFeedback.rubric_category_name }}</span>
-    </p>
+    <span class="s-h2 feedback-container__category--label">
+      {{ projectFeedback.rubric_category_name }}
+    </span>
     <div class="progress-bar" :style="{ width: projectFeedback.score * 80 + 'px'}">
       <div class="progress-bar__score-wrapper">
         <div class="progress-bar__score-wrapper--number">
@@ -10,7 +10,7 @@
         </div>
       </div>
     </div>
-    <p class="feedback-container__category--comments" s-text-body>
+    <p class="feedback-container__category--comments s-text-body">
       {{ projectFeedback.comment }}
     </p>
   </div>
