5ac2a5c835d5c08c5e1f1aeb87e99ce014d74591
Jessica Justice <jessbjust@gmail.com>
2021-05-25 02:03:37 -0600

    Hook up current student selected in state

--- src/store/index.js
+++ src/store/index.js
@@ -10,7 +10,8 @@ export default new Vuex.Store({
     allModules: [],
     currentProject: {},
     selectedProject: {},
-    myStudents: []
+    myStudents: [],
+    currentStudent: {}
   },
   mutations: {
     // mutations update state
@@ -25,6 +26,9 @@ export default new Vuex.Store({
     },
     setMyStudents (state, myStudents) {
       state.myStudents = myStudents
+    },
+    setCurrentStudent (state, foundStudent) {
+      state.currentStudent = foundStudent
     }
   },
   actions: {
@@ -54,8 +58,13 @@ export default new Vuex.Store({
         .then(response => response.json())
         .then(data => {
           context.commit('setMyStudents', data)
-          console.log(this.state.myStudents)
         })
+    },
+    setStudentData (context, studentId) {
+      const foundStudent = this.state.myStudents.data.find(student => student.id === studentId)
+      if (foundStudent) {
+        context.commit('setCurrentStudent', foundStudent)
+      }
     }
   },
   modules: {
@@ -78,6 +87,12 @@ export default new Vuex.Store({
     // },
     // getCurrentModule (state, getters) {
     // const sortedModules = this.state.allModules.sort((a, b) => a - b)
+    // }
+    // getStudentData: (state) => (studentId) => {
+    //   const foundStudent = state.myStudents.data.find(student => student.id === studentId)
+    //   if (foundStudent) {
+    //     context.commit('setCurrentStudent', foundStudent)
+    //   }
     // }
   }
 })
