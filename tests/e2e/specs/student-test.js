b36b3a1511db6021d24698023150b94f5c34b76d
Katie Bee <knees4bees@gmail.com>
2021-05-25 22:24:17 -0600

    Add beforeEach and increase wait time after fetches

--- tests/e2e/specs/student-test.js
+++ tests/e2e/specs/student-test.js
@@ -1,23 +1,23 @@
 // https://docs.cypress.io/api/introduction/api.html
 describe('Student dashboard', () => {
   // TODO beforeEach to .visit
+  beforeEach(() => {
+    cy.visit('http://localhost:8080/')
+  })
 
   it('Should have a header', () => {
-    cy.visit('http://localhost:8080/')
-      .get('img').should('have.class', 'logo rotating')
+    cy.get('img').should('have.class', 'logo rotating')
       .get('.date').contains(/\d\d\d\d/) //regex to find four digits in a row
       .get('h3').contains(/Hi\,\s[A-Z][a-z]/) //regex to find "Hi,", space, capital letter, lowercase letter
   })
 
   it('Should have a have a subheader', () => {
-    cy.visit('http://localhost:8080/')
-      .get('h2').contains('Welcome to Mesh.')
+    cy.get('h2').contains('Welcome to Mesh.')
       .get('p').contains('see your feedback')
   })
 
   it('Should have a sidebar', () => {
-    cy.visit('http://localhost:8080/')
-      .get('.left-section').find('a').should('have.length', 4)
+    cy.get('.left-section').find('a').should('have.length', 4)
       .get('.feedback-container').should('exist')
       .get('[data-cy=mod1').contains('Module 1')
       .get('[data-cy=mod2').contains('Module 2')
@@ -27,12 +27,11 @@ describe('Student dashboard', () => {
   })
 
   it('Should have a main feedback section', () => {
-    cy.visit('http://localhost:8080/')
-      .get('.feedback-container')
+    cy.get('.feedback-container')
       .get('[data-cy=mod2').click()
       .wait(1000)
       .get('[data-cy=mod1').click()
-      .wait(5000)
+      .wait(6000)
       .get('.project-tab').contains('Project 1')
       .get('.project-tab').contains('Project 2')
       .get('.project-tab').contains('Project 4')
@@ -54,11 +53,10 @@ describe('Student dashboard', () => {
   })
 
   it('Should accept and display user-entered notes', () => {
-    cy.visit('http://localhost:8080/')
-      .get('[data-cy=mod2').click()
+    cy.get('[data-cy=mod2').click()
       .wait(1000)
       .get('[data-cy=mod1').click()
-      .wait(5000)
+      .wait(6000)
       .get('.project-tab').contains('Project 2').click()
       .get('.current-notes').children().children().should('have.length', 0)
       .get('button').contains('Add new note').click()
@@ -72,8 +70,7 @@ describe('Student dashboard', () => {
   })
 
   it('Should have a footer', () => {
-    cy.visit('http://localhost:8080/')
-      .get('footer').contains('Ben')
+    cy.get('footer').contains('Ben')
       .get('footer').contains('Genevieve')
       .get('footer').contains('Jessica')
       .get('footer').contains('Jesus')
@@ -85,8 +82,7 @@ describe('Student dashboard', () => {
 
 describe('About page', () => {
   it('Should display a giant squid on the about page', () => {
-    cy.visit('http://localhost:8080/')
-      .get('.name').click()
+    cy.get('.name').click()
       .url().should('eq', 'http://localhost:8080/about')
       .get('h1').should('exist')
       .get('.squid').should('exist')
